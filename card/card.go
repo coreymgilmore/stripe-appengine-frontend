9b32de69818e55b65f43155b62cf1b78c743216e
Corey Gilmore - Work PC <coreymgilmore@gmail.com>
2015-10-09 13:40:34 -0400

    Try to fix bug where a user cannot add a new card for a just deleted customer.  For example: user deletes a customer b/c customer has a new card.  Delete completes successfully to user, but when a user tries to add the customer again (new card, same customer id), the user is told that the customer id is already in user.  Cache is not cleared for this customer Id (customer id is the memcache key).  How do we know cache is not cleared and this is the issue: easy, go to Dev Console and flush cache and everything works.  Added additional error handling to get more details to client when this occurs (user will not see details, but they will be in the http response).  Also make sure that if the customer id is not in the cache, we do not treat this as an error.  ErrCacheMiss is ok b/c we are deleting from the cache so if the item is not there, then this is successful.  Updated GAE version number since this is a reasonably big change if it works to fix the problem

--- card/card.go
+++ card/card.go
@@ -345,21 +345,35 @@ func Remove(w http.ResponseWriter, r *http.Request) {
 	stripeCustId := custData.StripeCustomerToken
 	sc.Customers.Del(stripeCustId)
 
-	//delete customer from memcache
-	//delete list of cards in memcache since this list is stale
-	memcache.Delete(c, datastoreId)
-	memcache.Delete(c, custData.CustomerId)
-	err = memcache.Delete(c, LIST_OF_CARDS_KEYNAME)
-	if err != nil {
-		output.Error(err, "There was an error flushing the cached list of cards.", w)
-		return
-	}
-
 	//delete custome from datastore
 	completeKey := getCustomerKeyFromId(c, datastoreIdInt)
 	err = datastore.Delete(c, completeKey)
 	if err != nil {
 		output.Error(err, "There was an error while trying to delete this customer. Please try again.", w)
+		return
+	}
+
+	//delete customer from memcache
+	//delete list of cards in memcache since this list is stale
+	//all memcache.Delete operations are listed first so error handling doesn't return if one fails...each call does not depend on another so this is safe
+	//obviously, if the card is not in the cache it cannot be removed (**this may be the solution to the below problem**)
+	//**bunch of err handling here to figure out why sometimes a card is not deleted from memcahce
+	//  user can remove card, but cannot add a card to the same Account ID (customerId)
+	//  for some reason the cache is not clearing this card's data that is being stored by customerID
+	//  even though the card is no longer in the datastore and a simple "flush cache" fixes the problem
+	err1 := memcache.Delete(c, datastoreId)
+	err2 := memcache.Delete(c, custData.CustomerId)
+	err3 := memcache.Delete(c, LIST_OF_CARDS_KEYNAME)
+	if err1 != nil && err1 != memcache.ErrCacheMiss{
+		output.Error(err1, "There was an error flushing this card's data from the cache (by datastore id). Please contact an administrator and have them flush the cache manually.", w)
+		return;
+	}
+	if err2 != nil && err2 != memcache.ErrCacheMiss{
+		output.Error(err2, "There was an error flushing this card's data from the cache (by customer id). Please contact an administrator and have them flush the cache manually.", w)
+		return
+	}
+	if err3 != nil && err3 != memcache.ErrCacheMiss{
+		output.Error(err3, "There was an error flushing the cached list of cards.", w)
 		return
 	}
 
